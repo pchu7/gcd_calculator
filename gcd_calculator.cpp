872cf3db8ae18896819f6621aa532c803a72f72e
Phil <philliprchu@gmail.com>
2020-07-05 23:17:58 -0500

    Added calculator functionality

--- gcd_calculator.cpp
+++ gcd_calculator.cpp
@@ -2,8 +2,19 @@
 
 using namespace std;
 
-int main() {
-	
+int main()
+{		
+	int x, y;
 
+	printf("Enter any two integers: ");
+	scanf("%d %d", &x, &y);
+	
+	while (x != y)
+	{
+		if (x > y) x -= y;
+		else y -= x;
+	}
+
+	printf("GCD = %d\n", x);
 	return 0;
 }
